dbf0833fad3ec9aa5863e2f2d205a4c49d58251d
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-03-02 18:31:50 +0100

    sistemato

--- js/script.js
+++ js/script.js
@@ -5,34 +5,29 @@ const container = document.querySelector('#container-campo-minato')
 btnPlay.addEventListener('click', function() {
     container.innerHTML = ''
     if (optDifficolta.value == 'facile') {
-        for (let i = 1; i <= 49; i++) {
-            const squareFacile = document.createElement('div');
-            squareFacile.classList.add('square-facile');
-            squareFacile.innerHTML = i
-            container.append(squareFacile)
-            squareFacile.addEventListener('click', function() {
-                squareFacile.classList.add('color-cell')
-            })
-        }
+        griglia(49)
     } else if (optDifficolta.value == 'medio') {
-        for (let i = 1; i <= 81; i++) {
-            const squareMedio = document.createElement('div');
-            squareMedio.classList.add('square-medio');
-            squareMedio.innerHTML = i
-            container.append(squareMedio)
-            squareMedio.addEventListener('click', function() {
-                squareMedio.classList.add('color-cell')
-            })
-        }
+        griglia(81)
     }else {
-        for (let i = 1; i <= 100; i++) {
-            const squareDifficile = document.createElement('div');
-            squareDifficile.classList.add('square-difficile');
-            squareDifficile.innerHTML = i
-            container.append(squareDifficile)
-            squareDifficile.addEventListener('click', function() {
-                squareDifficile.classList.add('color-cell')
-            })
-        }
+        griglia(100)
     }
 });
+
+function griglia(numero) {     
+    for (let i = 1; i <= numero; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.innerHTML = i;
+        container.append(square);
+        if (numero == 49) {
+            square.classList.add('facile');
+        }   else if (numero == 81) {
+            square.classList.add('medio');
+        }   else {
+            square.classList.add('difficile');
+        }
+        square.addEventListener('click', function() {
+            square.classList.add('color-cell')
+        })
+    }
+}
